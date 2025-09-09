4c5afc3c894e2598267b53054319db8c4d2aeedd
ArvinNotDev <arveenjafari19@gmail.com>
2025-09-09 12:20:44 +0330

    fixing save to json and load from json

--- core/register.py
+++ core/register.py
@@ -10,14 +10,20 @@ class CustomGamepad:
         self.load_from_json()
         
 
-    def save_to_json(self, name, map):
-        self.config[name] = map
-        json.dump(self.config, indent=4)
+    def save_to_json(self, name, mapping):
+        self.config[name] = mapping
+        with open("core/profiles/GenericGamepads.json", "w") as f:
+            json.dump(self.config, f, indent=4)
+
     
     def load_from_json(self):
-        with open("core/profiles/GenericGamepads.json", "r") as f:
-            self.config = json.load(f)
-            return self.config
+        try:
+            with open("core/profiles/GenericGamepads.json", "r") as f:
+                self.config = json.load(f)
+        except FileNotFoundError:
+            self.config = {}
+        return self.config
+
 
     def get(self, name):
         if name in self.config.keys():
