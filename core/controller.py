fe53159622634e7578e2bfeb28a2b91951ccd253
ArvinNotDev <arveenjafari19@gmail.com>
2025-11-13 00:58:26 +0330

    changing the name of each no named controller

--- core/controller.py
+++ core/controller.py
@@ -5,22 +5,27 @@ class PlayerType:
     def __repr__(self):
         return self.type
 
-
 class Controller:
     """
     Represents a physical controller.
     Unique ID is now the device path.
     """
 
+    NUMBER_OF_CONTROLLERS = 0
+
+    def __new__(cls):
+        NUMBER_OF_CONTROLLERS += 1
+        super.__new__()
+        
     def __init__(self, vendor_id, product_id, device_path, name=None):
         self.vendor_id = vendor_id
         self.product_id = product_id
         self.device_path = device_path
-        self.name = name if name else f"Controller-{vendor_id:04X}:{product_id:04X}"
+        self.name = name or f"Controller-{Controller.number_of_controllers}"
 
     @property
     def unique_id(self):
-        """Unique ID string based on VID:PID:path (optional, can just use path)"""
+        """Unique ID string based on device path"""
         return self.device_path
 
     def __repr__(self):
