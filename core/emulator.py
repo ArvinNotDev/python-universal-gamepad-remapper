577fec3f246eeaa0318824ea3a4f9cfd0ca1a7bf
ArvinNotDev <arveenjafari19@gmail.com>
2025-11-12 11:39:05 +0330

    adding press_release support to the emulator

--- core/emulator.py
+++ core/emulator.py
@@ -1,37 +1,30 @@
 import vgamepad as vg
 
 class EmulateX360:
-    v_x360 = vg.VX360Gamepad()
-
     def __init__(self, controller_uuid):
-        self.update_controller = controller_uuid
-        
+        self.controller_uuid = controller_uuid
+        self.v_x360 = vg.VX360Gamepad()
 
-    def update_controller(self, ljx, ljy, rjx, rjy, a, x, b, y, rb, rt, lb, lt, dpu, dpd, dpr, dpl):
+    def update(self, ljx, ljy, rjx, rjy, a=False, x=False, b=False, y=False,
+               rb=False, rt=0, lb=False, lt=0, dpu=False, dpd=False, dpr=False, dpl=False):
+
+        # Joysticks
         self.v_x360.left_joystick(x_value=int(ljx), y_value=int(ljy))
         self.v_x360.right_joystick(x_value=int(rjx), y_value=int(rjy))
 
+        # Triggers
         self.v_x360.left_trigger(value=int(lt))
         self.v_x360.right_trigger(value=int(rt))
 
-        if a: self.v_x360.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
-        else: self.v_x360.release_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
+        # Buttons
+        self._press_release(a, vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
+        self._press_release(b, vg.XUSB_BUTTON.XUSB_GAMEPAD_B)
+        self._press_release(x, vg.XUSB_BUTTON.XUSB_GAMEPAD_X)
+        self._press_release(y, vg.XUSB_BUTTON.XUSB_GAMEPAD_Y)
+        self._press_release(lb, vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_SHOULDER)
+        self._press_release(rb, vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_SHOULDER)
 
-        if b: self.v_x360.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_B)
-        else: self.v_x360.release_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_B)
-
-        if x: self.v_x360.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_X)
-        else: self.v_x360.release_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_X)
-
-        if y: self.v_x360.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_Y)
-        else: self.v_x360.release_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_Y)
-
-        if lb: self.v_x360.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_SHOULDER)
-        else: self.v_x360.release_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_SHOULDER)
-
-        if rb: self.v_x360.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_SHOULDER)
-        else: self.v_x360.release_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_SHOULDER)
-
+        # DPad
         if dpu:
             self.v_x360.directional_pad(direction=vg.DPAD_UP)
         elif dpd:
@@ -44,3 +37,9 @@ class EmulateX360:
             self.v_x360.directional_pad(direction=vg.DPAD_NONE)
 
         self.v_x360.update()
+
+    def _press_release(self, pressed, button):
+        if pressed:
+            self.v_x360.press_button(button=button)
+        else:
+            self.v_x360.release_button(button=button)
