ecff0bcbb735615184f9215c42f8625a406548ae
ArvinNotDev <arveenjafari19@gmail.com>
2025-12-17 21:55:59 +0330

    create settings automatically

--- core/settings.py
+++ core/settings.py
@@ -1,9 +1,33 @@
 import configparser
+from pathlib import Path
 
 class SettingsManager:
     def __init__(self, path="config/settings.conf"):
-        self.path = path
+        self.path = Path(path)
         self.config = configparser.ConfigParser()
+
+        if not self.path.exists():
+            self.path.parent.mkdir(parents=True, exist_ok=True)
+            self.config["device"] = {
+                "polling_rate": "2",
+                "auto_reconnect": "false",
+                "dpad_as_mouse": "true",
+                "left_stick_deadzone": "0.1",
+                "right_stick_deadzone": "0.1",
+            }
+            self.config["ui"] = {
+                "language": "eng",
+                "theme": "dark",
+            }
+            self.config["developer"] = {
+                "debug": "false",
+                "raw_hid_debug": "false",
+                "log_to_file": "false",
+                "log_file_path": "logs/mapper.log",
+            }
+            with self.path.open("w") as f:
+                self.config.write(f)
+        
         self.config.read(path)
 
     def get_device_section(self):
