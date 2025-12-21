4f5b8edd71aed23ab1e040f01a5c5351feea889f
ArvinNotDev <arveenjafari19@gmail.com>
2025-12-21 17:10:07 +0330

    stop instantiating the HidManager inside main

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from ui.main_window import MainWindow
-from core.hid import HIDManager
 
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("Universal Remapper")
-    hid_manager = HIDManager(poll_interval=0)
-    window = MainWindow(hid_manager, app)
+    window = MainWindow(app)
     window.show()
 
     sys.exit(app.exec())
